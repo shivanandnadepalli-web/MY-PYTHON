0113d871bcf6bdf403843bb945a5c22ce48f7454
shivanandnadepalli-web <shivanandnadepalli@gmail.com>
2025-11-12 16:03:22 +0530

    Create def_anyone.py

---
+++ def_anyone.py
@@ -0,0 +1,11 @@
+
+def anyone(x,y):
+ name=input("enter your name")
+ age=int(input("enter your age"))
+ rollnumber=int(input("enter you roll number"))
+ addition=age+rollnumber
+ print("the sum is:",addition)
+ mul=x*y
+ print("the multipliucation is",mul)
+ return name,age,rollnumber
+anyone (5,10)
