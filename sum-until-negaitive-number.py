3ffa47d3191e7ff7ab77b7ff842fdddb95af0cff
shivanandnadepalli-web <shivanandnadepalli@gmail.com>
2025-11-12 09:27:34 +0530

    Create sum-until-negaitive-number.py

---
+++ sum-until-negaitive-number.py
@@ -0,0 +1,10 @@
+#sum until negaitive number
+sum=0
+number = 0
+while number >=0:
+     number =int(input("enter a number(enter a negaitive numberstop):"))
+     if number >=0:
+          sum=sum +number
+print("The total sum of the non negaitivenumbers entered is:",sum)
+print("shivanand")
+print("AIML")
