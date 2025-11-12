d7369a958e8c366ea3e6cda7cc6f3b2434ca1852
shivanandnadepalli-web <shivanandnadepalli@gmail.com>
2025-11-12 11:28:27 +0530

    Create formated_string.py

---
+++ formated_string.py
@@ -0,0 +1,6 @@
+#formated string
+name="shivanand"
+age="20"
+college="SIET"
+depertment="AIML"
+print(f"my name is:{name} and my age is:{age}and iam studying at;{college} and my depertment is {depertment}")
